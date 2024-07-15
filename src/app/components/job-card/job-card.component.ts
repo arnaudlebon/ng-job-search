93441e21f6f4b59be85a8baaeb2e2b8861b339a1
Arnaud LEBON <alebon.pro@gmail.com>
2024-07-15 15:32:12 +0200

    fix icon star

--- src/app/components/job-card/job-card.component.ts
+++ src/app/components/job-card/job-card.component.ts
@@ -1,11 +1,12 @@
 import { Component, input } from '@angular/core';
 import { JobDetails } from '../../interfaces/jobs.interface';
 import { CommonModule } from '@angular/common';
+import { ToggleFavoriteDirective } from '../../directives/toggle-favorite.directive';
 
 @Component({
   selector: 'app-job-card',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ToggleFavoriteDirective],
   templateUrl: './job-card.component.html',
   styleUrl: './job-card.component.css'
 })
